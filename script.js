c6d52aa0c89322af15edb9fee25dd17ec5b521cb
nmaney <72935578+nmaney@users.noreply.github.com>
2021-03-15 22:32:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$('.navbar').addClass("sticky");
+		}else{
+			$('.navbar').removeClass("sticky");
+		}
+		if(this.scrollY > 500){
+			$('.scroll-up-btn').addClass("show");
+		}else{
+			$('.scroll-up-btn').removeClass("show");
+		}
+	});
+
+	//slide-up script
+	$('.scroll-up-btn').click(function(){
+		$('html, body').animate({scrollTop: 0 });
+	});
+	
+	//toggle menu/navbar script
+	$('.menu-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.menu-btn i').toggleClass("active");
+	});
+});
+	
+	//typing animation script
+	var typed= new Typed(".typing", {
+		strings: ["YouTuber", "Twitch Streamer", "Editor", "Aspiring Public Historian", "Freelancer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
